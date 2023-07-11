23705de87aca9763604a3a590605dcf7dbaa9fca
colocolov <colocolov.stas@gmail.com>
2023-07-11 16:40:35 +0300

    start hero slider

--- src/js/modules/main-slider.js
+++ src/js/modules/main-slider.js
@@ -16,39 +16,25 @@ new Swiper(_vars.heroSliderEl, {
   },
   //скорость переключения слайдов
   speed: 800,
-  // навигация по стрелкам
-  navigation: {
-    nextEl: ".main-slider__btn-next",
-    prevEl: ".main-slider__btn-prev",
-    // disabledClass: "stories-button__unactive",
-    clickable: true,
-    // для ппрвильного направления
-  },
-  // буллеты
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
-    dynamicBullets: true,
-  },
+
   //эффект перехода слайда (только если показ по 1-му слайду)
   effect: "fade",
   fadeEffect: {
     crossFade: true,
   },
   // показ кол-ва слайдов (работает, когда откл effect: "fade")
-  slidesPerView: 3.6,
+  // slidesPerView: 3.6,
   // расстояние между слайдами
-  spaceBetween: 40,
+  // spaceBetween: 40,
 
   // адаптив
-  breakpoints: {
-    // when window width is >= 320px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-  },
+  // breakpoints: {
+  //   // when window width is >= 320px
+  //   480: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  // },
 
   //отложенная загрузка:
   //отключаем презагрузку картинок
@@ -60,17 +46,17 @@ new Swiper(_vars.heroSliderEl, {
   // переключение при клике на слайд
   slideToClickedSlide: true,
   // отключение прокрутки при наведении мыши
-  on: {
-    init() {
-      this.el.addEventListener("mouseenter", () => {
-        this.autoplay.stop();
-      });
+  // on: {
+  //   init() {
+  //     this.el.addEventListener("mouseenter", () => {
+  //       this.autoplay.stop();
+  //     });
 
-      this.el.addEventListener("mouseleave", () => {
-        this.autoplay.start();
-      });
-    },
-  },
+  //     this.el.addEventListener("mouseleave", () => {
+  //       this.autoplay.start();
+  //     });
+  //   },
+  // },
   //
 });
 //----- END
