7d499f12a28f9e6d0f2d284c8b86ba695ba3e5fb
colocolov <colocolov.stas@gmail.com>
2023-07-13 16:35:41 +0300

    start sec new-good

--- src/js/_vars.js
+++ src/js/_vars.js
@@ -1,3 +1,4 @@
 export default {
   heroSliderEl: document.querySelector(".hero__slider"),
+  newGoodsEl: document.querySelector(".new-goods__product"),
 };
---
+++ src/js/modules/new-goods.js
@@ -0,0 +1,69 @@
+import _vars from "../_vars.js";
+import Swiper, { Pagination, Navigation, Autoplay } from "swiper";
+
+Swiper.use([Pagination, Autoplay]);
+
+// слайдер на главной
+new Swiper(_vars.newGoodsEl, {
+  loop: true,
+  // autoplay: {
+  //   //пауза между прокруткой
+  //   delay: 3000,
+  //   //закончить на последнем слайде
+  //   // stopOnLastSlide: false,
+  //   //отключить после ручного переключения
+  //   // disableOnInteraction: false,
+  // },
+  //скорость переключения слайдов
+  // speed: 800,
+  navigation: {
+    nextEl: ".new-goods--next",
+    prevEl: ".new-goods--prev",
+    // disabledClass: "stories-button__unactive",
+    clickable: true,
+    // для ппрвильного направления
+  },
+
+  //эффект перехода слайда (только если показ по 1-му слайду)
+  // effect: "fade",
+  // fadeEffect: {
+  //   crossFade: true,
+  // },
+  // показ кол-ва слайдов (работает, когда откл effect: "fade")
+  slidesPerView: 4,
+  // расстояние между слайдами
+  spaceBetween: 1,
+
+  // адаптив
+  // breakpoints: {
+  //   // when window width is >= 320px
+  //   480: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //   },
+  // },
+
+  //отложенная загрузка:
+  //отключаем презагрузку картинок
+  preloadImages: false,
+  lazy: {
+    loadOnTransitionStart: false,
+    loadPrevNext: false,
+  },
+  // переключение при клике на слайд
+  // slideToClickedSlide: true,
+  // отключение прокрутки при наведении мыши
+  // on: {
+  //   init() {
+  //     this.el.addEventListener("mouseenter", () => {
+  //       this.autoplay.stop();
+  //     });
+
+  //     this.el.addEventListener("mouseleave", () => {
+  //       this.autoplay.start();
+  //     });
+  //   },
+  // },
+  //
+});
+//----- END
