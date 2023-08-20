9ee7d674a63550f0cd6344cd6a617844ae70423c
Stas Colocolov <colocolov.stas@gmail.com>
2023-08-20 23:14:24 +0300

    upd quantity no-active js

--- src/js/modules/quantity.js
+++ src/js/modules/quantity.js
@@ -1,3 +1,17 @@
+
+// отключения минуса при значении 1
+const valueDefaul = document.querySelectorAll('.quantity__input');
+  if (valueDefaul.length) {
+    valueDefaul.forEach((item) => {
+      if (item.children[0].value == 1) {
+        let minusBtn = item.closest('.quantity').querySelector('.quantity__btn_minus');
+        minusBtn.classList.add('no-active');
+      }
+    });
+  }
+
+
+// работа счетчика
 let quantityBtns = document.querySelectorAll('.quantity__btn');
 if (quantityBtns.length > 0) {
   for (let index = 0; index < quantityBtns.length; index++) {
@@ -5,13 +19,13 @@ if (quantityBtns.length > 0) {
     quantityBtn.addEventListener('click', function (e) {
       let value = parseInt(quantityBtn.closest('.quantity').querySelector('input').value);
       if (quantityBtn.classList.contains('quantity__btn_plus')) {
-        document.querySelector('.quantity__btn_minus').classList.remove('no-active');
+        quantityBtn.closest('.quantity').querySelector('.quantity__btn_minus').classList.remove('no-active');
         value++;
       } else {
         --value;
         if (value <= 1) {
           value = 1;
-          document.querySelector('.quantity__btn_minus').classList.add('no-active');
+          quantityBtn.closest('.quantity').querySelector('.quantity__btn_minus').classList.add('no-active');
         }
       }
       quantityBtn.closest('.quantity').querySelector('input').value = value;
